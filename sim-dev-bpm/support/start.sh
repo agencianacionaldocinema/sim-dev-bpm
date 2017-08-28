eb572318e9cc04f0808410e112b323a0832ea8ca
Diogo Alves <diogo.alves@ancine.gov.br>
2017-08-28 14:34:53 -0300

    first commit

---
+++ sim-dev-bpm/support/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+BUILD_DIR=/opt/jboss/src
+OUTPUT_DIR=/opt/jboss/eap/standalone/deployments
+
+if [ ! -d  $DATA_DIR/.niogit ]; then
+    cp -a /opt/jboss/repositorio-local-niogit $DATA_DIR/.niogit 
+fi
+
+if [ $DATA_DIR/.niogit ] && [ ! -d  $DATA_DIR/.niogit/local.git ]; then
+    rm -rf $DATA_DIR/.niogit
+    cp -Rf /opt/jboss/repositorio-local-niogit $DATA_DIR/.niogit/
+fi
+
+cd /opt/jboss/src/sin-bpm
+git remote add bc-host ../../data/.niogit/local.git
+git remote add bc-container /opt/jboss/data/.niogit/local.git
+cd /opt/jboss
+
+CLEAN_REPO_SIZE="`du -sb /opt/jboss/repositorio-local-niogit/local.git | cut -f1`"
+DATA_REPO_SIZE="`du -sb /opt/jboss/data/.niogit/local.git | cut -f1`"
+if [ "$DATA_REPO_SIZE" -le "$CLEAN_REPO_SIZE" ] ; then
+    cd /opt/jboss/src/sin-bpm
+    git push -f -u bc-container `git rev-parse --abbrev-ref HEAD`:master 
+    cd /opt/jboss/
+fi
+
+mvn install -DskipTests -f $BUILD_DIR/sin-bpm/pom.xml
+
+/opt/jboss/eap/bin/standalone.sh -c standalone.xml -b 0.0.0.0 -bmanagement 0.0.0.0
